bfedd6a0a7cbab158cece12b776c103415e58c95
Eduardo Lopez Biagi <eduardo.biagi@gmail.com>
2014-01-20 21:55:45 -0800

    Update some profiles.clj dependencies

--- any/lein/profiles.clj
+++ any/lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-swank "1.4.5"]
-                  [lein-light "0.0.16"]
+                  [lein-light-nrepl "0.0.10"]
                   [lein-marginalia "0.7.1"]
                   ;[lein-noir "1.2.1"]
-                  [lein-iclojure "1.1"]
-                  [lein-kibit "0.0.7"]]}}
+                  [lein-iclojure "1.2"]
+                  [lein-kibit "0.0.8"]]}}
