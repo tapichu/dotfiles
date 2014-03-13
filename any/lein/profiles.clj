e222645f2e7e3ff11ecd63c4f8c92b84f57f75c1
Eduardo Lopez Biagi <biagic@amazon.com>
2014-03-13 13:55:08 -0700

    Assing eastwood lein plugin

--- any/lein/profiles.clj
+++ any/lein/profiles.clj
@@ -1,6 +1,8 @@
-{:user {:plugins [[lein-swank "1.4.5"]
-                  [lein-light-nrepl "0.0.10"]
-                  [lein-marginalia "0.7.1"]
-                  ;[lein-noir "1.2.1"]
+{:user {:plugins [[jonase/eastwood "0.1.0"]
                   [lein-iclojure "1.2"]
-                  [lein-kibit "0.0.8"]]}}
+                  [lein-kibit "0.0.8"]
+                  [lein-light-nrepl "0.0.16"]
+                  [lein-marginalia "0.7.1"]
+                  ;[lein-swank "1.4.5"]
+                  ;[lein-noir "1.2.1"]
+                 ]}}
