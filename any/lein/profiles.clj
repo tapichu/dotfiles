952bd8e0875434a2f0c9c0b9f9366aeec2060528
Eduardo Lopez Biagi <eduardo.biagi@gmail.com>
2015-06-08 20:42:01 -0700

    Update lein dependencies

--- any/lein/profiles.clj
+++ any/lein/profiles.clj
@@ -1,15 +1,13 @@
 {:user {:plugins [[cider/cider-nrepl "0.8.2"]
-                  [codox "0.8.11"]
+                  [codox "0.8.12"]
                   [jonase/eastwood "0.2.1"]
                   [lein-datomic "0.2.0"]
                   [lein-iclojure "1.2"]
-                  [lein-kibit "0.0.8"]
+                  [lein-kibit "0.1.2"]
                   [lein-light-nrepl "0.1.0"]
                   [lein-marginalia "0.8.0"]
-                  ;[lein-swank "1.4.5"]
-                  ;[lein-noir "1.2.1"]
-                  [refactor-nrepl "1.0.1"]
+                  [refactor-nrepl "1.0.5"]
                  ]
         ;:datomic {:install-location "/Users/tapichu/Downloads/datomic-free-0.9.4699"}}}
         :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.7"]]}}
+                       [org.clojure/tools.nrepl "0.2.10"]]}}
